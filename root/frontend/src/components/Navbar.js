873b5afb3e840c4ff6b5bd77782f861a32bdb6ef
Sadie Burns <sadiejklol@gmail.com>
2024-10-01 23:48:10 -0400

    added the remaining drop down menus, just need to fix the visual bug when you click the drop down menus

--- root/frontend/src/components/Navbar.js
+++ root/frontend/src/components/Navbar.js
@@ -6,46 +6,67 @@ export default function Navbar() {
     return <nav className="nav">
         <Link to="/" className="site-logo">[logo img]</Link>
         <ul>
-            <li>
+            <li id="element">
                 <CustomLink to="/">Home</CustomLink>
             </li>
-            <li>
                 <div className="dropdown">
-                    <div className="dropbtn">
+                    <div id="dropbtn">
                         <CustomLink to="/about">About <ChevronIcon className="chevron-icon" /></CustomLink>
                     </div>
                     <div className="dropdown-content">
-                        <li className="first-element">
+                        <li className="first-element" id="element">
                              <CustomLink to="/about">Scholarships</CustomLink>
                         </li>
-                        <li>
+                        <li id="element">
                             <CustomLink to="/about">Policies</CustomLink>
                         </li>
-                        <li>
+                        <li id="element">
                             <CustomLink to="/about">Grants</CustomLink>
                         </li>
                     </div>
                 </div>
+            <div className="dropdown">
+                <div id="dropbtn" style={{zIndex: "1"}}>
+                    <CustomLink to="/roster">Team Roster <ChevronIcon className="chevron-icon" /></CustomLink>
+                </div>
+                <div className="dropdown-content">
+                    <li className="first-element" id="element">
+                        <CustomLink to="/roster">Coaches</CustomLink>
+                    </li>
+                    <li id="element">
+                        <CustomLink to="/roster">Members</CustomLink>
+                    </li>
+                    <li id="element">
+                        <CustomLink to="/roster">Success Stories</CustomLink>
+                    </li>
+                </div>
+            </div>
+            <div className="dropdown">
+                <div id="dropbtn" style={{zIndex: "1"}}>
+                    <CustomLink to="/competition">Competition <ChevronIcon className="chevron-icon" /></CustomLink>
+                </div>
+                <div className="dropdown-content">
+                    <li className="first-element" id="element">
+                        <CustomLink to="/competition">Schedule</CustomLink>
+                    </li>
+                    <li id="element">
+                        <CustomLink to="/competition">Past Awards</CustomLink>
+                    </li>
+                </div>
+            </div>
+            <li style={{zIndex: "1"}} id="element">
+                <CustomLink to="/press" >Press</CustomLink>
             </li>
-            <li style={{zIndex: "1"}}>
-                <CustomLink to="/roster">Team Roster <ChevronIcon className="chevron-icon" /></CustomLink>
-            </li>
-            <li>
-                <CustomLink to="/competition">Competition <ChevronIcon className="chevron-icon" /></CustomLink>
-            </li>
-            <li>
-                <CustomLink to="/press">Press</CustomLink>
-            </li>
-            <li>
+            <li id="element">
                 <CustomLink to="/gallery">Gallery</CustomLink>
             </li>
-            <li>
+            <li id="element">
                 <CustomLink to="/faq">FAQ</CustomLink>
             </li>
-            <li>
+            <li id="element">
                 <CustomLink to="/support">Support</CustomLink>
             </li>
-            <li>
+            <li id="element">
                 <CustomLink to="/contact">Contact</CustomLink>
             </li>
         </ul>
