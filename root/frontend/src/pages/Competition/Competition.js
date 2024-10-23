4aa80fbace4284d8359bee0bad62c346c808e860
Sadie Burns <sadiejklol@gmail.com>
2024-10-22 20:35:23 -0400

    finished competitions page (for now...responsive and backend components)

--- root/frontend/src/pages/Competition/Competition.js
+++ root/frontend/src/pages/Competition/Competition.js
@@ -1,6 +1,15 @@
 import './Competition.css'
 import TopPic from './toppic.png'
+import Circle from './eclipse.png'
+import Twenty from './2020.png'
+import TwentyOne from './2021.png'
+import TwentyTwo from './2022.png'
+import TwentyThree from './2023.png'
+import TwentyFour from './2024.png'
 import PresPic from './president.png'
+import firstPlace from './1st-place.png'
+import thirdPlace from './3rd-place.png'
+import secondPlace from './2nd-place.png'
 import CompPhoto from './competitionlogo.png'
 import React, { useEffect } from 'react'
 import { Container, Row, Col } from "react-bootstrap"
@@ -128,6 +137,153 @@ const Competition = () => {
       </Row>
       <Row className='awards'>
         <Col>
+          <div className='awards-title'>
+            <h1>AWARDS AND RECOGNITIONS</h1>
+          </div>
+        </Col>
+        <Row className='awards-medals'>
+          <Col className='champs text-center'>
+            <div className='first-place'>
+              <img src={firstPlace} />
+            </div>
+            <div className='medal text-center'>
+              <div className='circle'>
+                <img src={Circle} />
+              </div>
+            </div>
+            <div className='champs-num'>
+              <h1>5</h1>
+            </div>
+            <div className='champs-title'>
+              <h5>CCDC National<br />Championships</h5>
+            </div>
+            <div className='champs-date'>
+              <p>2014, 2015, 2016, 2021, 2022</p>
+            </div>
+          </Col>
+          <Col className='runner-up-1 text-center'>
+            <div className='second-place'>
+              <img src={secondPlace} />
+            </div>
+            <div className='medal text-center'>
+              <div className='circle'>
+                <img src={Circle} />
+              </div>
+            </div>
+            <div className='champs-num'>
+              <h1>3</h1>
+            </div>
+            <div className='champs-title'>
+              <h5>CCDC National<br />Runner-Up Finishes</h5>
+            </div>
+            <div className='champs-date'>
+              <p>2018, 2019, 2020</p>
+            </div>
+          </Col>
+          <Col className='runner-up-2 text-center'>
+            <div className='third-place'>
+              <img src={thirdPlace} />
+            </div>
+            <div className='medal text-center'>
+              <div className='circle'>
+                <img src={Circle} />
+              </div>
+            </div>
+            <div className='champs-num'>
+              <h1>3</h1>
+            </div>
+            <div className='champs-title'>
+              <h5>CCDC National<br />Runner-Up Finishes</h5>
+            </div>
+            <div className='champs-date'>
+              <p>2018, 2019, 2020</p>
+            </div>
+          </Col>
+        </Row>
+      </Row>
+      <Row className='timeline'>
+        <Col className='col-1'>
+          <h1>CCDC@UCF OVER RECENT YEARS</h1>
+          <svg width="100" height="5" viewBox="0 0 100 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0H100V5H0V0Z" fill="#FFC904"/>
+          </svg>
+          <div className='col-1-video'>
+              <h1>2024</h1>
+              <p> Proin cras id semper tellus euismod aenean est a egestas.<br />Rutrum in ipsum vivamus nunc dignissim sit.<br />Proin cras id s sit.....</p>
+              <iframe
+              src="https://www.youtube.com/embed/tgbNymZ7vqY">
+              </iframe>
+          </div>
+          <div className='col-2-video'>
+              <h1>2022</h1>
+              <p> Proin cras id semper tellus euismod aenean est a egestas.<br />Rutrum in ipsum vivamus nunc dignissim sit.<br />Proin cras id s sit.....</p>
+              <img src={PresPic} />
+          </div>
+          <div className='col-3-video'>
+              <h1>2024</h1>
+              <p> Proin cras id semper tellus euismod aenean est a egestas.<br />Rutrum in ipsum vivamus nunc dignissim sit.<br />Proin cras id s sit.....</p>
+              <iframe
+              src="https://www.youtube.com/embed/tgbNymZ7vqY">
+              </iframe>
+          </div>
+        </Col>
+        <Col>
+          <div className='twenty-four text-center'>
+            <img src={TwentyFour} />
+          </div>
+          <div className='twenty-four-div text-center'>
+            <svg width="3" height="301" viewBox="0 0 3 301" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="2" height="300" transform="matrix(-1 0 0 1 2.08789 0.373047)" fill="#6C757D"/>
+            </svg>
+          </div>
+          <div className='twenty-three text-center'>
+            <img src={TwentyThree} />
+          </div>
+          <div className='twenty-three-div text-center'>
+            <svg width="3" height="301" viewBox="0 0 3 301" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="2" height="300" transform="matrix(-1 0 0 1 2.08789 0.373047)" fill="#6C757D"/>
+            </svg>
+          </div>
+          <div className='twenty-two text-center'>
+            <img src={TwentyTwo} />
+          </div>
+          <div className='twenty-two-div text-center'>
+            <svg width="3" height="301" viewBox="0 0 3 301" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="2" height="300" transform="matrix(-1 0 0 1 2.08789 0.373047)" fill="#6C757D"/>
+            </svg>
+          </div>
+          <div className='twenty-one text-center'>
+            <img src={TwentyOne} />
+          </div>
+          <div className='twenty-one-div text-center'>
+            <svg width="3" height="301" viewBox="0 0 3 301" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="2" height="300" transform="matrix(-1 0 0 1 2.08789 0.373047)" fill="#6C757D"/>
+            </svg>
+          </div>
+          <div className='twenty text-center'>
+            <img src={Twenty} />
+          </div>
+        </Col>
+        <Col className='col-2'>
+         <div className='col-4-video'>
+              <h1>2022</h1>
+              <p> Proin cras id semper tellus euismod aenean est a egestas.<br />Rutrum in ipsum vivamus nunc dignissim sit.<br />Proin cras id s sit.....</p>
+              <img src={PresPic} />
+          </div>
+          <div className='col-5-video'>
+              <h1>2024</h1>
+              <p> Proin cras id semper tellus euismod aenean est a egestas.<br />Rutrum in ipsum vivamus nunc dignissim sit.<br />Proin cras id s sit.....</p>
+              <iframe
+              src="https://www.youtube.com/embed/tgbNymZ7vqY">
+              </iframe>
+          </div>
+        </Col>
+      </Row>
+      <Row className='history'>
+        <Col>
+          <div className='history-title'>
+            <h1>MORE COMPETITION HISTORY</h1>
+          </div>
         </Col>
       </Row>
     </Container>
